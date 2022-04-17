f87c0a088f06008e476f78dd4028b340b40ea977
tecnolisolutions <tecnoli.solutions@outlook.com>
2022-04-17 12:13:25 -0600

    adicion de codigo base

---
+++ src/logica/Logica_mock.py
@@ -0,0 +1,9 @@
+class Logica_mock():
+
+    def __init__(self):
+        #Constructor con datos de ancho y largo
+        self.lenght1=100
+        self.widt1=20
+        self.lenght2 = 50
+        self.widt2 = 10
+        self.lengh_width=[{'Lenght':30, 'Width': 5}, {'Lenght':50, 'Width': 2}]
